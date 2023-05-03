5f7a6d8087c4a592cb57d88c43ee6b003be4a997
Kaustubh Dighe <kaustubhdighe1@gmail.com>
2023-05-03 17:00:26 -0400

    Fixed loop ordering

--- src/applications/gen-mappings/generate_mappings.cpp
+++ src/applications/gen-mappings/generate_mappings.cpp
@@ -26,16 +26,16 @@ std::vector<std::vector<int> > getForLoopOrderings(std::vector<int> dims) {
     return perms;
 }
 
+// TODO: 5/3 embed workload parsing
 int main(/*int argc, char* argv[]*/) {
     std::vector<FusedMapping> mappings;
     int m2_id = 0, n2_id = 1, k2_id  = 2; // all permutations
     std::vector<int> dims = {m2_id, n2_id, k2_id};
     std::vector<std::vector<int> > orderings;
     do {
-        FusedMapping mapping;
-        std::cout << "Storage DRAM ID: " << mapping.AddChild<Storage>(1, 0, 0) << std::endl; // DRAM
-        mapping.AddChild<For>(2, "it1", 0); // 0 is dim_id
-        mapping.AddChild<Storage>(3, 0, 0); // storage for Z1
+        // std::cout << "Storage DRAM ID: " << mapping.AddChild<Storage>(1, 0, 0) << std::endl; // DRAM
+        // mapping.AddChild<For>(2, "it1", 0); // 0 is dim_id
+        // mapping.AddChild<Storage>(3, 0, 0); // storage for Z1
         
         std::vector<int> this_dim = dims;
         orderings.push_back(this_dim);
@@ -43,16 +43,18 @@ int main(/*int argc, char* argv[]*/) {
             std::cout << dim << ' ';
         }
         std::cout << std::endl;
-        for(size_t i = 0; i < dims.size() - 1; i++) {
+        for(size_t i = 0; i < dims.size(); i++) {
+            FusedMapping mapping;
             for(size_t j = 0; j < dims.size(); j++) {
                 if(j == i) {
-                    mapping.AddChild<Storage>(j+3, 0, 0);
+                    mapping.AddChild<Storage>(j+1, 0, 0);
                     continue;
                 }
-                int p_id = (int) (j < i ? j + 2 : j + 3);
+                int p_id = (int) (j < i ? j : j + 1);
                 mapping.AddChild<For>(p_id, "it" + (char)('0' + p_id), dims[j]);
             }
+            mappings.push_back(mapping);
         }
-        mappings.push_back(mapping);
     } while(std::next_permutation(dims.begin(), dims.end()));
+    std::cout << mappings.size() << std::endl;
 }
