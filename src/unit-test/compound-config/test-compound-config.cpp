9c29b65de4fc180128c44d1ff95dc1c3deb9e6c3
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-06-14 18:26:51 +0000

    compiled testing skeleton

--- src/unit-test/compound-config/test-compound-config.cpp
+++ src/unit-test/compound-config/test-compound-config.cpp
@@ -1,23 +1,52 @@
 #define BOOST_TEST_MODULE TestCompoundConfig
 #include <boost/test/included/unit_test.hpp>
+#include <compound-config/compound-config.hpp>
 
 // number of testing cycles to run
 int TESTS = 2000;
 // the seed for the entropy source
 uint SEED = 42;
 // changes the max random value to the U_LONG32 random value
-#define RAND_MAX = ULONG_LONG_MAX;
+#undef RAND_MAX
+#define RAND_MAX = ULONG_LONG_MAX
 
+// static YAML file names we want to load in for the test
+std::vector<std::string> FILES = {
+    "example.yaml" // include source here if pulled from a repo somewhere
+};
+
+
+// we are only testing things in config
+namespace config {
 // tests the lookup functions when reading in from file
 BOOST_AUTO_TEST_CASE(testLookups)
 {
-    // creates the entropy source
-    srand(SEED);
-    // tracks the test cases
-    std::vector<long long> tests;
-    for (int test = 0; test < TESTS; test++)
+    for (auto FILE:FILES) 
     {
-        tests.push_back(rand());
+        // reads the YAML file into CompoundConfig
+        CompoundConfig cConfig = CompoundConfig(FILE, "yaml");
+        // reads in the YAML file independently of CompoundConfig
+        YAML::Node testRef = YAML::LoadFile(FILE);
+
+        // gets the root CompoundConfigNode
+        config::CompoundConfigNode root = cConfig.getRoot();
+
+        // goes through all keys and compares the values.
+        // figure this out. you may need a helper file.
+        for (auto node: testRef)
+        {
+            // unpacks the key + expected value
+            std::string key, expected;
+            key = node.first.as<std::string>();
+            expected = node.second.as<std::string>();
+
+            // unpacks actual value
+            std::string actual;
+            root.lookupValue(key, actual);
+
+            // compares to the config
+            BOOST_CHECK_EQUAL(actual, expected);
+        }
     }
 }
 
@@ -32,3 +61,4 @@ BOOST_AUTO_TEST_CASE(testLookupDynamics)
 {
 
 }
+} // namespace config
