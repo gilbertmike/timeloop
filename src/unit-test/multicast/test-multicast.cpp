4f0089094d498d8dd891797eb4a6f7bfb58da165
Renggeng Zheng <rzhengcontacts@gmail.com>
2024-11-20 19:46:42 -0500

    string formatting not supported

--- src/unit-test/multicast/test-multicast.cpp
+++ src/unit-test/multicast/test-multicast.cpp
@@ -1,5 +1,6 @@
 #include <ctime>
 #include <iostream>
+#include <format>
 #include <string>
 
 #include <boost/test/unit_test.hpp>
@@ -198,6 +199,108 @@ BOOST_AUTO_TEST_CASE(TestDistributedMulticastHyperCubeModel)
   }
   std::cout << "DistributedMulticastHyperCubeModel Test Passed" << std::endl;
 }
+
+BOOST_AUTO_TEST_CASE(CollectDataMulticastHyperCubeModel)
+{
+  using namespace analysis;
+  ///@brief Strings necessary for the data collection.
+  // Global to buffer maps.
+  std::string fill_buffers_template = R"FILL({
+    noc[tm, tn, xd, yd] -> A[m, k] : 
+      0 <= tm < 8 and 0 <= tn < 8 and tn = 0 and 
+      0 <= xd < 8 and 0 <= yd < 8 and 
+      m = (8 * tm) + xd and 0 <= k < 64 and
+      yd * {0} == (k * {0}) % 8
+  })FILL";
+  std::string occ_global_str = R"OCC({
+    noc[tm, tn, xs, ys] -> A[m, k] : 
+      0 <= tm < 8 and 0 <= tn < 8 and 
+      xs=0 and ys=0 and 
+      0 <= m < 64 and 0 <= k < 64
+  })OCC";
+  std::string dist_g2b_func = R"DIST({
+    [noc[tm, tn, xd, yd] -> noc[tm, tn, xs, ys]] -> hops[(xd - xs) + (yd - ys)] : 
+      xd >= xs and yd >= ys;
+    [noc[tm, tn, xd, yd] -> noc[tm, tn, xs, ys]] -> hops[-(xd - xs) + -(yd - ys)] : 
+      xd < xs and yd < ys;
+    [noc[tm, tn, xd, yd] -> noc[tm, tn, xs, ys]] -> hops[-(xd - xs) + (yd - ys)] : 
+      xd < xs and yd >= ys;
+    [noc[tm, tn, xd, yd] -> noc[tm, tn, xs, ys]] -> hops[(xd - xs) + -(yd - ys)] : 
+      xd >= xs and yd < ys
+  })DIST";
+  // Buffer to PE maps.
+  std::string fill_pes_str = R"FILL({
+    noc[tm, tn, tk, xd, yd] -> A[m, k] : 
+      0 <= tm < 8 and 0 <= tn < 8 and 0 <= tk < 64 and 
+      0 <= xd < 8 and 0 <= yd < 8 and
+      m= (8 * tm) + xd and k=tk
+  })FILL";
+  std::string occ_buffers_template = R"OCC({
+    noc[tm, tn, tk, xs, ys] -> A[m, k] : 
+      0 <= tm < 8 and 0 <= tn < 8 and 0 <= tk < 64 and 
+      0 <= xs < 8 and 0 <= ys < 8  and 
+      m= (8 * tm) + xs and 0 <= k < 64 and
+      yd * {0} == (k * {0}) % 8
+  })OCC";
+  std::string dist_b2p_str = R"DIST({
+    [noc[tm, tn, tk, xd, yd] -> noc[tm, tn, tk, xs, ys]] -> hops[(xd - xs) + (yd - ys)] : 
+      xd >= xs and yd >= ys;
+    [noc[tm, tn, tk, xd, yd] -> noc[tm, tn, tk, xs, ys]] -> hops[-(xd - xs) + -(yd - ys)] : 
+      xd < xs and yd < ys;
+    [noc[tm, tn, tk, xd, yd] -> noc[tm, tn, tk, xs, ys]] -> hops[-(xd - xs) + (yd - ys)] : 
+      xd < xs and yd >= ys;
+    [noc[tm, tn, tk, xd, yd] -> noc[tm, tn, tk, xs, ys]] -> hops[(xd - xs) + -(yd - ys)] : 
+      xd >= xs and yd < ys
+  })DIST";
+
+  ///@brief Static objects 
+  // Global to buffer maps.
+  isl::map occ_global = isl::map(GetIslCtx(), occ_global_str);
+  auto g2b_dims = std::vector<SpaceTime>{
+    Temporal(), Temporal(), Spatial(0, 0), Spatial(1, 0)
+  };
+  Occupancy occ_global_obj = Occupancy(g2b_dims, occ_global);
+  isl::map dist_g2b = isl::map(GetIslCtx(), dist_g2b_func);
+  auto g2b_model = DistributedMulticastHypercubeModel(true, dist_g2b);
+  // Buffer to PE maps.
+  isl::map fill_pes = isl::map(GetIslCtx(), fill_pes_str);
+  auto b2p_dims = std::vector<SpaceTime>{
+    Temporal(), Temporal(), Temporal(), Spatial(0, 0), Spatial(1, 0)
+  };
+  Fill fill_pes_obj = Fill(b2p_dims, fill_pes);
+  isl::map dist_b2p = isl::map(GetIslCtx(), dist_b2p_str);
+  auto b2p_model = DistributedMulticastHypercubeModel(
+    true, dist_b2p
+  );
+  
+  std::cout << "Collecting Hypercube Data Test" << std::endl;
+  for (int d = 0; d < 8; ++d) {
+    ///@brief Constructs the 
+    int buf_id = 0;
+    ///@brief Construct the necessary Timeloop objects.
+    Fill fill_buffers = Fill(
+      g2b_dims,
+      isl::map(GetIslCtx(), std::format(fill_buffers_template, d))
+    );
+    Occupancy occ_buffers = Occupancy(
+      b2p_dims,
+      isl::map(GetIslCtx(), std::format(occ_buffers_template, d))
+    );
+    ///@brief Apply the models.
+    TransferInfo g2p_info = g2b_model.Apply(buf_id, fill_buffers, occ_global_obj);
+    TransferInfo b2p_info = b2p_model.Apply(buf_id, fill_pes_obj, occ_buffers);
+    ///@brief Check the results
+    long g2p_hops = isl::manage(isl_pw_qpolynomial_eval(
+      g2p_info.p_hops, isl_point_zero(isl_pw_qpolynomial_get_domain_space(g2p_info.p_hops))
+    )).get_num_si();
+    long b2p_hops = isl::manage(isl_pw_qpolynomial_eval(
+      b2p_info.p_hops, isl_point_zero(isl_pw_qpolynomial_get_domain_space(b2p_info.p_hops))
+    )).get_num_si();
+    std::cout << "D: " << d << " | G2P Hops: " << g2p_hops << " | B2P Hops: " << b2p_hops << std::endl;
+  }
+  std::cout << "DistributedMulticastHyperCubeModel Test Passed" << std::endl;
+}
+
 /*
 BOOST_AUTO_TEST_CASE(TestDistributedMulticastOrderedExtentsDORModel)
 {
