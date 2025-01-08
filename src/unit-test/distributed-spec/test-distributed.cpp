6d63b98ae8a674264473f0d673b18b09556e9fe7
Renggeng Zheng <rzhengcontacts@gmail.com>
2025-01-08 14:32:45 -0500

    made TopoSpec test more comprehensive

--- src/unit-test/distributed-spec/test-distributed.cpp
+++ src/unit-test/distributed-spec/test-distributed.cpp
@@ -14,17 +14,30 @@ BOOST_AUTO_TEST_CASE(TestNocFromYaml)
   YAML::Node file = YAML::LoadFile("./src/unit-test/distributed-spec/distributed.yaml");
   for (const auto& root: file)
   {
+    // Gets this topology spec.
     TopologySpec spec = topology_from_yaml(root.first.as<std::string>(), root.second);
+    BOOST_ASSERT(
+      spec.name == root.first.as<std::string>()
+    );
+
+    // Verifies the dimensions of the topology.
+    std::vector<std::string> dims = root.second["dims"].as<std::vector<std::string>>();
+    BOOST_ASSERT(
+      dims == spec.dims
+    );
+
+    // Computes the pw_multi_aff for the NoC cost.
     isl::pw_multi_aff aff = isl::pw_multi_aff(GetIslCtx(), root.second["affine"].as<std::string>());
-    isl::pw_multi_aff map = isl::pw_multi_aff(GetIslCtx(), root.second["domain_restriction"].as<std::string>());
     BOOST_ASSERT(
       isl_pw_multi_aff_is_equal(
         spec.noc_cost.copy(), aff.copy()
       )
     );
+
+    // Verifies the domain restrictions.
     isl::set input_domain  = spec.domain.product(spec.domain);
     isl::pw_multi_aff restricted = spec.noc_cost.intersect_domain(input_domain);
-    std::cout << input_domain << std::endl;
+    isl::pw_multi_aff map = isl::pw_multi_aff(GetIslCtx(), root.second["domain_restriction"].as<std::string>());
     BOOST_ASSERT(
       isl_pw_multi_aff_is_equal(
         restricted.copy(), map.copy()
